e28dc5fc5473f90e7ce9eb57337fca9397aafdab
romainb22 <Bruno.Romain@etu.univ-grenoble-alpes.fr>
2021-04-24 15:52:37 +0200

    tried to implement loading function but segfault

--- main.c
+++ main.c
@@ -3,10 +3,46 @@
 #include <math.h>
 #include<time.h>
 #include "point.h"
+#define MAX_LINE_LENGTH 15
+
+point ** loadFromfile(char * path){
+  FILE *f;
+  char c[MAX_LINE_LENGTH];
+  int nbPoints, dimension, nbClasses,i,classe;
+  float x,y;
+
+  i = -1;
+  f = fopen(path, "r");
+  while (fgets(c,MAX_LINE_LENGTH, f)){
+    if(i==-1){
+      sscanf(c,"%d %d %d", &nbPoints, &dimension, &nbClasses);
+    }
+    else{
+      /* Do nothing */
+    }
+    i++;
+  }
+  point * tab[nbPoints];
+  i = -1;
+  while (fgets(c, MAX_LINE_LENGTH, f)){
+    if(i==-1){
+      /* Do nothing */
+    }
+    else{
+      sscanf(c,"%d %f %f", &classe, &x, &y);
+      tab[i]= newPoint(x, y, classe);
+    }
+    i++;
+  }
+  for(i=1;i<nbPoints;i++){
+    printf("%f\n",getDistance(tab[i-1],tab[i]));
+  }
+  return tab;
+}
 
 int main(){
 srand(time(NULL));
-point * tableau[50];
+point ** tableau;
 point *p1;
 point *p2;
 double x=0.5;
@@ -19,14 +55,15 @@ p1=newPoint(x,y,class);
 p2=newPoint(x2,y2,c);
 getDistance(p1,p2);
 printf("%f\n",getDistance(p1,p2));
-for(i=0;i<50;i++){
+/*for(i=0;i<50;i++){
   x = (double) rand()/RAND_MAX*2.0-1.0;
   y = (double) rand()/RAND_MAX*2.0-1.0;
   tableau[i] = newPoint(x,y,i%2+1);
 }
 for(i=0;i<49;i++){
   printf("%f\n",getDistance(tableau[i],tableau[i+1]));
-}
+}*/
+tableau = loadFromfile("/example/ex1.txt");
 
 exit(EXIT_SUCCESS);
 }
