247417303085a209d6058d55e4342e0ca5176e7f
Hanybay <hanybayazid11@gmail.com>
2021-05-05 11:06:34 +0200

    ajout des fonctions affichage

--- main.c
+++ main.c
@@ -5,12 +5,34 @@
 #include "point.h"
 #include "tableau.h"
 #include "load.h"
+#include "affichage.h"
+#include <MLV/MLV_all.h>
 #define MAX_LINE_LENGTH 15
 
 int main(){
-  srand(time(NULL));
+/*  srand(time(NULL));
   tableau *tab;
-  tab = loadFromfile("./example/ex1.txt");
+  tab = loadFromfile("./example/ex1.txt"); */
+
+MLV_create_window("test","iconetest",1000,1000);
+/*MLV_draw_rectangle(50,100,400,450,MLV_COLOR_BLUE);*/
+mode_creation(1000,1000);
+mode_kppv(1000,1000);
+vakeur_k(1000, 1000,"lourd");
+rein_fenetre(1000, 1000);
+zone_affichage(1000, 1000);
+efface_dernier_point(1000, 1000);
+classe_manuel(1000, 1000);
+option_affichage(1000, 1000);
+voisinage(1000, 1000);
+avec_prise_decision(1000, 1000);
+save_donnees(1000, 1000);
+charge_fichier(1000,1000);
+
+MLV_actualise_window();
+MLV_wait_seconds(20);
+MLV_free_window();
+
 
 exit(EXIT_SUCCESS);
 }
